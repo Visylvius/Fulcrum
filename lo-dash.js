9090f45a487bb0ce11c190d6b936ed8f69d86bb3
Loren Barrick <lorenbarrick@gmail.com>
2015-11-30 18:47:01 -0800

    add each, map, every, some, filter, reduce, contains, indexOf

---
+++ lo-dash.js
@@ -0,0 +1,81 @@
+// each
+function each(collection, callback) {
+  if (Array.isArray(collection)) {
+    for (var i = 0; i < collection.length; i++) {
+      callback(collection[i]);
+    }
+  } else {
+    for (var prop in collection) {
+      callback(collection[prop]);
+    }
+  }
+}
+//Map
+function map(collection, callback) {
+  var result = [];
+  each(collection, function(element) {
+    result.push(callback(element));
+  });
+  return result;
+}
+//filter
+function filter(collection, callback) {
+  var result = [];
+  each(collection, function(element) {
+    if (callback(element)) {
+      result.push(element);
+    }
+  });
+  return result;
+}
+//some
+function some(collection, callback) {
+  var result = false;
+  each(collection, function(element) {
+    if (callback(element)) {
+      result = true;
+    }
+  });
+  return result;
+}
+//every
+function every(collection, callback) {
+  var result = true;
+  each(collection, function(element) {
+    if (!callback(element)) {
+      result = false;
+    }
+  });
+  return result;
+}
+//reduce
+function reduce(collection, callback, initial) {
+  var accumulator = initial;
+  each(collection, function(element) {
+    if (accumulator === undefined) {
+      accumulator = element;
+    } else {
+      accumulator = callback(accumulator, element);
+    }
+  });
+  return accumulator;
+}
+//contains
+function contains(collection, target) {
+  return reduce(collection, function(accumulator, element) {
+    return accumulator || element === target;
+  }, false);
+}
+//indexOf
+function indexOf(collection, value, fromIndex) {
+  var result = -1;
+  var position = fromIndex -1 || 0;
+  fromIndex = fromIndex || 0;
+  each(collection, function(element) {
+    if ((element === target) && (result === -1) && (position >= fromIndex)) {
+        result = position;
+    }
+    position++;
+  });
+  return result;
+}
